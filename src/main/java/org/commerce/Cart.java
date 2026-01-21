ea6c641b0fc27477e3b12699fb0d28a828d3a9e4
jasmin <cjm98@smartel.co.kr>
2026-01-21 19:32:47 +0900

    Refactor : totalPrice를 외부에서 데이터를 접근하거나, 제어할 수 없도록 변경. [additional][최재민]

--- src/main/java/org/commerce/Cart.java
+++ src/main/java/org/commerce/Cart.java
@@ -4,62 +4,65 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Cart {
-    // 카트에서 꼭 제품 객채를 들고있어야 할까..?
-    // 그냥 제품 아이디와 해당 제품의 갯수만 가지고 있어도 될거같은데...
-    // 그렇다고 그냥 id 와 quantity만 가지고있다면..? 객체를 리스트로 만들어야하니 이는 절대 X
-    //private List<Product> productList;
-    // 그럼 id 와 quantity를 가지고있는 cartItem 객체를 만들자.
     private final List<CartItem> items = new ArrayList<>();
     private int totalPrice = 0;
 
+    /// productId 로 cart안에 담긴 상품 제품 제거.
     public void removeItem(String productId) {
-        //이미 저장된 데이터가 있는지 확인
-        Optional<CartItem> isCartItem = findItemByProductId(productId);
+        Optional<CartItem> isCartItem = findCartItemByProductId(productId);
         isCartItem.ifPresentOrElse(item -> {
             items.remove(item);
-            this.totalPrice -= item.getProductQuantity() * item.getProductPrice();
+            manageTotalPrice(-(item.getProductQuantity() * item.getProductPrice()));
         } , () -> System.out.println("이미 장바구니에 없는 아이템입니다."));
     }
 
+    ///  카트에 상품을 추가, 이미 상품이있다면 갯수만 증가합니다.
     public void addItemToCart(String productId, int quantity , String productName , int productPrice) {
         //이미 저장된 데이터가 있는지 확인
-        Optional<CartItem> isCartItem = findItemByProductId(productId);
+        Optional<CartItem> isCartItem = findCartItemByProductId(productId);
         isCartItem.ifPresentOrElse(item -> {
             // 있으면 기존 카트에 갯수만 추가.
             // 새로운 객체 생성 X
             item.manageProductQuantity(quantity);
         }, //없으면 새로운 카트 아이템 추가
                 () -> items.add(new CartItem(productId,productName,productPrice,quantity)));
-        this.totalPrice += quantity * productPrice;
+        manageTotalPrice(quantity * productPrice);
         System.out.println("장바구니 추가 완료!");
     }
 
+    /// 카트에 담긴 물건의 수량을 조절합니다.
     public void manageItemQuantity(String productId, int itemCount) {
-        Optional<CartItem> isCartItem = findItemByProductId(productId);
+        Optional<CartItem> isCartItem = findCartItemByProductId(productId);
         isCartItem.ifPresentOrElse(i -> {
                     i.manageProductQuantity(itemCount);
-                    this.totalPrice += itemCount * i.getProductPrice();
+                    manageTotalPrice(itemCount * i.getProductPrice());
                 },
                 () -> System.out.println("장바구니에 존재하지 않은 제품입니다."));
     }
 
-//    이렇게 작성하면 객체에 직접적으로 영향을 줄 수 있기떄문에 다른 방식으로 캡슐화를 하는것이 좋다고 피드백을 받음.
-//    public List<CartItem> getItems() {
-//        return items;
-//    }
+    /// 제품 Id로 카트 안에 담긴 제품을 조회. 다른 곳에서
+    private Optional<CartItem> findCartItemByProductId(String productId) {
+        return items.stream().filter(i -> productId.equals(i.getProductId())).findFirst();
+    }
+
+    /// 현재 카트의 총 결제 금액을 관리하는 메서드.
+    /// 다른곳에서 관리 할 수 없도록 막기.
+    private void manageTotalPrice(int totalPrice) {
+        if(this.totalPrice + totalPrice <= 0){
+            this.totalPrice = 0;
+            return;
+        }
+        this.totalPrice += totalPrice;
+    }
+
     public List<CartItem> getItems() {
         return Collections.unmodifiableList(items);
     }
 
+    // primitive 이기때문에 어차피 복사본을 반환해서 복사해서 반환 할 필요 XX
     public int getTotalPrice() {
         return totalPrice;
     }
-
-    // 중복된 코드가있어서 그냥 메서드로 분리함.
-    private Optional<CartItem> findItemByProductId(String productId) {
-        return items.stream().filter(i -> productId.equals(i.getProductId())).findFirst();
-    }
-
     @Override
     public String toString() {
         int totalAmount = items.stream()
--- src/main/java/org/commerce/CartItem.java
+++ src/main/java/org/commerce/CartItem.java
@@ -2,8 +2,6 @@ package org.commerce;
 
 public class CartItem {
     private final String productId;
-    // 이름과 가격같은 최소한의 정보는 추가적으로 들고있는게 시스템 상 편해서 추가적으로 작성.
-    // 없을경우 또 조회해서 이름이랑 가격을 찾아서 반환해줘야하는데 그것보단 이름과 가격을들고있는게 합리적이라고 생각했습니다.
     private final String productName;
     private final int productPrice;
     private int productQuantity;
@@ -41,7 +39,7 @@ public class CartItem {
     @Override
     public String toString() {
         return """
-                id =%s | 상품명 = %s | 가격 = %,d | 수량 = %d
+                id = %s | 상품명 = %s | 가격 = %,d | 수량 = %d
                 """.formatted(productId ,productName, productPrice, productQuantity);
     }
 }
